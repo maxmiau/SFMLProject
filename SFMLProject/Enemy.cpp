e9dca239255f0afa8a9dd96321123787f680120c
maxmiau <miaumax@gmail.com>
2020-07-09 20:01:56 -0500

    Añadiendo Enemigos

---
+++ SFMLProject/Enemy.cpp
@@ -0,0 +1,25 @@
+#include "Enemy.h"
+
+Enemy::Enemy() {}
+Enemy::~Enemy() {}
+
+void Enemy::init(std::string textureName, sf::Vector2f position, float _speed) {
+	m_speed = _speed;
+	m_position = position;
+
+	// Cargar la textura
+	m_texture.loadFromFile(textureName.c_str());
+
+	// Crear sprite y configurar la textura
+	m_sprite.setTexture(m_texture);
+	m_sprite.setPosition(m_position);
+	m_sprite.setOrigin(m_texture.getSize().x / 2, m_texture.getSize().y / 2);
+}
+
+void Enemy::update(float dt) {
+	m_sprite.move(m_speed * dt, 0);
+}
+
+sf::Sprite Enemy::getSprite() {
+	return m_sprite;
+}
--- SFMLProject/Source.cpp
+++ SFMLProject/Source.cpp
@@ -1,10 +1,16 @@
 #include "SFML-2.5.1/include/SFML/Graphics.hpp"
+#include <vector>
+
 #include "Hero.h"
+#include "Enemy.h"
 
 // Manejo de ventana
 sf::Vector2f viewSize(1024, 768);
 sf::VideoMode vm(viewSize.x, viewSize.y);
 sf::RenderWindow window(vm, "Test", sf::Style::Default);
+
+// Crear enemigos
+void spawnEnemy();
 
 // Manejo de los sprites
 sf::Texture skyTexture;
@@ -16,6 +22,12 @@ sf::Sprite bgSprite;
 // Instancia del heroe
 Hero hero;
 
+// Vector donde se almacenan los enemigos en pantalla
+std::vector<Enemy*> enemies;
+
+// Frecuencia de generacion de los enemigos
+float currentTime;
+float prevTime = 0.0f;
 
 
 
@@ -31,6 +43,8 @@ void init()
 
 	hero.init("Assets/graphics/hero.png", sf::Vector2f(viewSize.x * 0.25f,
 		viewSize.y * 0.5f), 200);
+
+	srand((int)time(0));
 }
 
 // Funciones del teclado
@@ -56,6 +70,11 @@ void draw()
 	window.draw(skySprite);
 	window.draw(bgSprite);
 	window.draw(hero.getSprite());
+
+	for (Enemy *enemy : enemies)
+	{
+		window.draw(enemy->getSprite());
+	}
 	
 }
 
@@ -63,6 +82,27 @@ void draw()
 void update(float dt)
 {
 	hero.update(dt);
+
+	currentTime += dt;
+	// crear enemigos
+	if (currentTime >= prevTime + 1.125f) {
+		spawnEnemy();
+		prevTime = currentTime;
+	}
+
+	// Eliminar enemigos del vector
+	for (int i = 0; i < enemies.size(); i++)
+	{
+		Enemy *enemy = enemies[i];
+
+		enemy->update(dt);
+
+		if (enemy->getSprite().getPosition().x < 0)
+		{
+			enemies.erase(enemies.begin() + i);
+			delete(enemy);
+		}
+	}
 }
 
 int main()
@@ -90,4 +130,35 @@ int main()
 	}
 
 	return EXIT_SUCCESS;
+}
+
+void spawnEnemy() {
+	int randLoc = rand() % 3;
+
+	sf::Vector2f enemyPos;
+
+	float speed;
+
+	switch (randLoc)
+	{
+	case 0: 
+		enemyPos = sf::Vector2f(viewSize.x, viewSize.y * 0.75f);
+		speed = -400; break;
+
+	case 1:
+		enemyPos = sf::Vector2f(viewSize.x, viewSize.y * 0.60f);
+		speed = -550; break;
+
+	case 2:
+		enemyPos = sf::Vector2f(viewSize.x, viewSize.y * 0.40f);
+		speed = -650; break;
+
+	default:
+		printf("Incorrect 'y' value \n"); return;
+	}
+
+	Enemy* enemy = new Enemy();
+	enemy->init("Assets/graphics/enemy.png", enemyPos, speed);
+
+	enemies.push_back(enemy);
 }
